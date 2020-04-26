be012222581e0168e08e0dd7b2d11538808610f2
stanislavchetinov7 <63706570+stanislavchetinov7@users.noreply.github.com>
2020-04-26 18:20:01 +0300

    Add files via upload

---
+++ robot/src/robot/robota2.java
@@ -0,0 +1,85 @@
+
+import java.util.Scanner;
+
+public class robota2 {
+
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+String robot = ("Napishi vida na obekta:");
+int stena, stol, nishto;
+System.out.println("Forward");
+System.out.println("Jump");
+System.out.println("Go Sideway");
+boolean stenaFrontOfRobot = true;
+boolean stolFrontOfRobot = true;
+boolean nishtoFrontOfrobot = true;
+
+if(stenaFrontOfRobot) {
+System.out.println("Turn left or right");
+
+}if (stolFrontOfRobot){
+System.out.println("Jump it");
+
+}else if(nishtoFrontOfrobot) {
+System.out.println("Go ahead");
+}
+
+int fullBatteryLife = 1;
+int mouseSensitivityWork = 1;
+int fightSkills = 1;
+
+if(fullBatteryLife > 1) {
+System.out.println("Good condition");
+}
+if(mouseSensitivityWork > 1) {
+System.out.println("Good condition");
+}
+if(fightSkills > 1) {
+System.out.println("Battle fighter");
+
+int NumberOfPunches = 4;
+while(NumberOfPunhes = 4) {
+System.out.println("The robot will make the fight");
+
+int[] numbers = {1,2,3,4,5,6,7,8,9,10};
+for(x = 1; x !>10; x++) {
+System.out.println()
+int y = 1;
+
+
+
+}
+}
+
+
+
+
+
+
+}
+
+
+
+
+	
+
+
+
+
+	
+	
+	
+
+
+	
+	
+	
+	
+	
+}	
+	
+
+	
+	
+
+}
