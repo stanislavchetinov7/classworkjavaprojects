9e7a298960f7beaff33d5527c88348caac588d72
stanislavchetinov7 <63706570+stanislavchetinov7@users.noreply.github.com>
2020-05-04 15:41:10 +0300

    Add files via upload

---
+++ domashnarabota8/src/domashnarabota8/domashnarabota8.java
@@ -0,0 +1,57 @@
+package domashnarabota8;
+import java.Util.Scanner;
+public class domashnarabota8 {
+
+public static void main(String[] args) {
+
+Scanner scanner = new Scanner(System.in);
+
+String game = "Carqt e martav";
+int  numberOFDw;
+int numberOfM;
+int numberOfD;
+int numberOfK;
+int numberOf;
+
+String Dw = "djudjeta";
+String M = "kartechnica";
+String D = "magare";
+String K  = "tsar";
+String Q = "tsaritsa";
+int t = 0;
+int g = 0;
+public static gameColor {
+for(int t = 1; i <=6; i++) {
+for(int g = 1; g <=6; g++) {
+setColor(Color.Black);
+setFill(i,g,6,6);
+int y = 0;
+}
+}
+
+}
+
+switch(Dw = 2)
+case 1;
+System.out.println("Dvijenie s edno kvadratche napred");
+break;
+case 2;
+System.out.println("Vrashtane nazad");
+
+if(y <=0) {
+System.out.println("Game Over");
+}
+
+}
+
+
+
+}
+
+
+
+
+	
+	
+	
+
---
+++ domashnarabota8/src/module-info.java
@@ -0,0 +1,2 @@
+module domashnarabota8 {
+}
