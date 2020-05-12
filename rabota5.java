68f71621600ddd1b406a6e303ed89fb4645910f4
stanislavchetinov7 <63706570+stanislavchetinov7@users.noreply.github.com>
2020-05-12 13:58:53 +0300

    Add files via upload

---
+++ rabota5.java
@@ -0,0 +1,71 @@
+package rabota;
+import java.util.Scanner;
+	
+public class rabota5 {
+
+public class NumberList {
+
+	boolean ifProgramisOn = true;
+	boolean ifProgramisOff = false;
+	
+	
+	
+		
+	
+	}
+	
+	public static void main(String args[]) {
+	
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the array: ");
+		
+		int arraySize = input.nextInt();
+		int[] array = new int[arraySize];
+		
+		
+		
+		
+}
+	
+		
+	public static void TypingArray(int arraySize, int [] array) {
+	
+		for(int i = 0; i < arraySize; i++) {
+		Scanner input = new Scanner(System.in);
+		int TheNum;
+		System.out.println("Enter currency  for: ");
+		
+		
+		
+				
+		}
+		
+	}
+	
+	
+	public static void MekeAChoice(int arraySize, int[] array) {
+	
+	Scanner input = new Scanner (System.in);
+	int choice = input.nextInt();
+	switch (choice) {
+	
+	case 1:
+	TypingArray(arraySize, array);
+	
+	case 2:
+	
+	
+	
+	}
+	
+	
+	}
+	
+	
+	
+	
+		
+		
+	}
+
+
