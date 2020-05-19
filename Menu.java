bd720f22341b72e615d0a8008f52cc7468c89ca3
stanislavchetinov7 <63706570+stanislavchetinov7@users.noreply.github.com>
2020-05-19 21:42:54 +0300

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,81 @@
+package domashnarabota17;
+import java.util.Scanner;
+
+public class Menu {
+
+		
+	
+public void MenuOptions() {
+
+
+
+
+
+
+}
+
+public void EndProgram () {
+
+	boolean exitProgram = true;
+	while(true) {
+
+		System.out.println("Krai na programata");
+	}	
+	
+	
+}
+
+	public static void getWordsMenu () {
+
+	System.out.println(" 1.Obarnete bukvite na dumite ot masiva naobratno i gi vizualiziraite v konzolata\n" +
+					   "2.Izkaraite broq na povtarqshtite se simvoli za vsqka edna ot dumite v masiva\n" +
+			           "3. Izvedete broq na simvolite za vsqka edna ot dumite v masiva\n" +
+					   "4.Izvedete broq na povtarqshtite se dumi v masiva\n" +
+			           "5.Vrashtane nazad kam osnovnoto menu");
+	}
+	
+	public static void getNumberMenu () {
+	System.out.println("1.Izvejdane samo na prostite chisla v masiva\n" +
+					  "2. Izvejdane na nai-chesto sreshtan element v masiva\n" +
+					  "3. Izvejdane na masksimalna redica ot narastvashti elementi v masiva\n" +
+					  "4. Izvejdane na maksimalna redica ot namalqvashti elementi v masiva\n" +
+					  "5. Izvejdane na maksimalna redica ot ednakvi elementi v masiva\n" +
+					  "6. Izvejdane na posledovatelnost ot chisla ot chisla ot masiva, koito imat suma ravna na chislo, generirano na sluchaen princip\n" +
+					  "7. Vrashtane nazad kam osnovnoto menu");
+	}
+	
+	
+	
+
+	
+	public static void BackToMenu() {
+	
+	System.out.println("1. Rabota s chisla\n" +
+						"2.Rabota s dumi\n" +
+						"3.Izhod ot programata");
+		
+
+
+	}
+
+
+	public static  int TheNumber (int min) {
+		Scanner scanner = new Scanner(System.in);
+		boolean ifIncorrect = true;
+		int options = 0;
+		
+			
+		
+		
+		}
+	
+	
+	}
+	
+}
+
+
+
+	
+ 
+	
