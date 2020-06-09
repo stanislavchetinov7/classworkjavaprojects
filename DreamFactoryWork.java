64cf7eb135b2ced8a347e9f64a8d711012037ba8
stanislavchetinov7 <63706570+stanislavchetinov7@users.noreply.github.com>
2020-06-09 13:03:08 +0300

    Add files via upload

---
+++ DreamFactoryWork.java
@@ -0,0 +1,122 @@
+package company;
+
+
+public class DreamFactoryWork {
+
+public static void main(String[] args) {	
+
+
+    
+	
+	      
+}
+
+public class Machine{
+	
+int power;
+int height;
+String color;
+	
+}
+
+
+public abstract class HeatManager {
+    protected int power;
+    protected double maxTemperature;
+
+    public HeatManager(int power, double maxTemperature) {
+        this.power = power;
+        this.maxTemperature = maxTemperature;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public double getMaxTemperature() {
+        return maxTemperature;
+    }
+
+    public void setMaxTemperature(double maxTemperature) {
+        this.maxTemperature = maxTemperature;
+    }
+}
+
+
+public class WrapperEngine extends HeatManager {
+
+
+    public WrapperEngine(int power, double maxTemperature) {
+        super(power, maxTemperature);
+    }
+}
+
+
+public class Engine extends Machine{
+
+
+    public Engine(int power, double height, String color) {
+        
+    }
+
+}
+
+
+
+}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+		
+
+
+
+
+
+
+
+	
+
+	
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
